770b5881210eec18cec2c929cf95c79c83092d30
Gerson Lopez <galdl1438@gmail.com>
2022-10-05 12:13:54 -0500

    Se agregaron y actualizaron todos los archivos, 05/10/22

---
+++ reto2/script.js
@@ -0,0 +1,20 @@
+/*Formato losless,
+     no tienen perdida de calidad las imagenes
+    
+     Pero suelen ser imagenespesadas
+
+        GIF
+        png 8 (256 colores)
+        png 24 mas pesado
+
+  Formato lossy
+    Pesan menos y tiene menor calidad y menos peso
+    carga mas rapido el navegor  
+
+    JPG
+    SVG-Vector
+    
+    TAMANO optimo 70 KB
+
+    Verifix elimina los metadatos de una imagen
+    */
