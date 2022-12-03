30213e5ff956ebe78061fa40b63c157fbb8f3685
Gerson Lopez <galdl1438@gmail.com>
2022-12-03 14:34:24 -0600

    Se finalizo la parte del curso de HTML, los siguientes commits seran de CSS

---
+++ reto8/script.js
@@ -0,0 +1,4 @@
+var subBoton = document.getElementById("bs");
+var boton = document.getElementById("b");
+subBoton.addEventListener("click",function(){alert("Presionaste el boton SUBMIT")});
+boton.addEventListener("click",function(){alert("Presionaste el boton normal")});
